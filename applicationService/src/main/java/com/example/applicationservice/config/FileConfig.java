27e804728428424f88d980f6c28ebb2ae472cb0d
Mary Yang <66921052+goldfishtelbooth@users.noreply.github.com>
2024-08-26 11:47:47 -0700

    uploadFilesToS3

---
+++ applicationService/src/main/java/com/example/applicationservice/config/FileConfig.java
@@ -0,0 +1,33 @@
+package com.example.applicationservice.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FileConfig {
+
+    @Value("${aws.access.key}")
+    private String awsAccessKey;
+
+    @Value("${aws.secret.key}")
+    private String awsSecretKey;
+
+    @Value("${aws.s3.region}")
+    private String awsRegion;
+
+    @Bean
+    public AmazonS3 s3Client() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.fromName(awsRegion))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+    }
+}
+
---
+++ applicationService/src/main/java/com/example/applicationservice/controller/FileController.java
@@ -0,0 +1,23 @@
+package com.example.applicationservice.controller;
+
+import com.example.applicationservice.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileController {
+
+    @Autowired
+    private FileService fileService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileUrl = fileService.uploadFile(file);
+        return new ResponseEntity<>(fileUrl, HttpStatus.OK);
+    }
+}
+
