515e71d602428c52c1a3e28d0382a811a0ffa2ac
Yizheng <yw6429@nyu.edu>
2024-08-28 20:03:11 -0400

    UPDATE: spring security configs

--- HousingService/src/main/java/com/housingservice/config/SecurityConfig.java
+++ HousingService/src/main/java/com/housingservice/config/SecurityConfig.java
@@ -1,17 +1,30 @@
 package com.housingservice.config;
 
+import com.housingservice.security.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private JwtFilter jwtFilter;
+
+    @Autowired
+    public void setJwtFilter(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll()
+                .antMatchers("/api/**").hasAnyAuthority("HR")
                 .anyRequest().authenticated();
     }
 }
---
+++ HousingService/src/main/java/com/housingservice/security/JwtFilter.java
@@ -0,0 +1,45 @@
+package com.housingservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+//The jwt filter that we want to add to the chain of filters of Spring Security
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    public void setJwtProvider(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Optional<AuthUserDetail> authUserDetailOptional = jwtProvider.resolveToken(request); // extract jwt from request, generate a userdetails object
+
+        if (authUserDetailOptional.isPresent()){
+            AuthUserDetail authUserDetail = authUserDetailOptional.get();
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    authUserDetail.getUsername(),
+                    null,
+                    authUserDetail.getAuthorities()
+            ); // generate authentication object
+
+            SecurityContextHolder.getContext().setAuthentication(authentication); // put authentication object in the secruitycontext
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
