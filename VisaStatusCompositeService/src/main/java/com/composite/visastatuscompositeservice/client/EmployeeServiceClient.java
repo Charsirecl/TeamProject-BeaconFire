22607cf3b3374d055114f6e19bd7fc1c36b9d411
Mary Yang <66921052+goldfishtelbooth@users.noreply.github.com>
2024-08-30 02:51:24 -0700

    update Employee Service url

--- VisaStatusCompositeService/src/main/java/com/composite/visastatuscompositeservice/client/EmployeeServiceClient.java
+++ VisaStatusCompositeService/src/main/java/com/composite/visastatuscompositeservice/client/EmployeeServiceClient.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "employee-service", url = "http://localhost:8080") // Replace with your EmployeeService URL
+@FeignClient(name = "EmployeeService", url = "http://localhost:8080") // Replace with your EmployeeService URL
 public interface EmployeeServiceClient {
 
-    @PutMapping("/api/employees/personalDocument/{employeeId}")
+    @PutMapping("EmployeeService/api/employees/personalDocument/{employeeId}")
     ResponseEntity<Void> updatePersonalDocument(@PathVariable("employeeId") String employeeId,
                                                 @RequestBody PersonalDocument document);
 
-    @PutMapping("/api/employees/visaStatus/{employeeId}")
+    @PutMapping("EmployeeService/api/employees/visaStatus/{employeeId}")
     ResponseEntity<Void> updateVisaStatus(@PathVariable("employeeId") String employeeId,
                                           @RequestBody VisaStatus visaStatus);
 }
