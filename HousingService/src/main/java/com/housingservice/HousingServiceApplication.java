dcde6f99d4c3a246f18751b05e75c835cd22c2e8
Yizheng <yw6429@nyu.edu>
2024-08-29 20:23:38 -0400

    Update eureka and gateway settings

--- HousingService/src/main/java/com/housingservice/HousingServiceApplication.java
+++ HousingService/src/main/java/com/housingservice/HousingServiceApplication.java
@@ -2,10 +2,12 @@ package com.housingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class HousingServiceApplication {
 
     public static void main(String[] args) {
